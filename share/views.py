4664de69155d0427d63ef51f8c4a3b4f05c1b26c
jaune <jaune.pixel@gmail.com>
2019-04-15 17:55:32 +0200

    fix

--- share/views.py
+++ share/views.py
@@ -13,16 +13,20 @@ def index_share(request):
     # general mood
     moods = UserMood.objects.values('mood__number').annotate(c=Count('mood__number')) \
         .order_by('-c', '-mood__number').first()
-    m = moods['mood__number']
+    if moods is None:
+        m = int(0)
+    else:
+        m = moods['mood__number']
     # top five
     music_a, music_n = top_author(1), top_name(1)
     book_a, book_n = top_author(2), top_name(2)
     serie_n = top_name(3)
     # question
     top = Topic.objects.last()
-    top_val = Topic.objects.filter(pk=top.id).values('subject', 'reply_1', 'reply_2')
-    reply_count = Choice.objects.filter(topic_id=top.id).values('topic__subject', 'topic__choice__submit')\
-        .annotate(c=Count('topic__choice__submit')).order_by('-c')[:1]
+    if top is not None:
+        top_val = Topic.objects.filter(pk=top.id).values('subject', 'reply_1', 'reply_2')
+        reply_count = Choice.objects.filter(topic_id=top.id).values('topic__subject', 'topic__choice__submit')\
+            .annotate(c=Count('topic__choice__submit')).order_by('-c')[:1]
     return render(request, 'share/dash_share.html', locals())
 
 
