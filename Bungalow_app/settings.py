12486816a9e90d6421ac1d32fb9a2d81b0ce64f1
jaune <jaune.pixel@gmail.com>
2019-04-15 16:42:37 +0200

    refactor

--- Bungalow_app/settings.py
+++ Bungalow_app/settings.py
@@ -155,18 +155,4 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
 # Logging
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
+
